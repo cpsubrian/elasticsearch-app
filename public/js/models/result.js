f22c59265d1d63a7c3f6f1ee386a31b8d7e2fcf3
erinlesavoy <erin.lesavoy@gmail.com>
2012-08-30 15:47:13 -0400

    Adds results model validator and more styling

--- public/js/models/result.js
+++ public/js/models/result.js
@@ -5,6 +5,20 @@ define(function(require) {
   var Result = Model.extend({
     defaults: {
       name: 'default-result'
+    },
+    validate: function(attrs){
+      if(!attrs.type)
+        return "Result must define its type";
+
+      if(attrs.type === 'people'){
+        if(!attrs.name || !attrs.tag_line || !attrs.connections
+          || !attrs.regions || !attrs.issues)
+          return "Person missing required attribute(s)";
+      }else if (attrs.type === 'articles'){
+        if(!attrs.title || !attrs.text || !attrs.date || !attrs.sections
+          || !attrs.tags || !attrs.url);
+          return "Article missing required attribute(s)";
+      }
     }
   });
 
